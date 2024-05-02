79cc547bd7daf5ae6b33e70af8e6ad1f2d19b43e
javierh-castro <javi2cc1@gmail.com>
2024-05-02 14:45:52 -0300

    Third commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import "./App.css";
 // import Dark from "./dark";
 
 function App() {
-  const [dark, setDark] = useState(false);
+  const [dark, setDark] = useState(() => {
+    const modeDark = window.localStorage.getItem('mode')
+    return modeDark ? JSON.parse(modeDark) : false
+  });
 
   useEffect(() => {
     const handleScroll = () => {
@@ -28,6 +31,8 @@ function App() {
       document.body.classList.remove("dark-mode");
     }
   };
+  
+  window.localStorage.setItem('mode', JSON.stringify(dark))
 
   return (
     <>
@@ -70,8 +75,11 @@ function App() {
           </label>
           <input type="checkbox" id="toggle" /> */}
           <div className="dynamic">
-            <label onClick={darkMode}>
-              <i className={dark ? "fa-solid fa-sun" : "fa-solid fa-moon"}></i>
+            <input type="checkbox" className="input-check" id="darkmode-toggle"/>
+            <label htmlFor="darkmode-toggle" className="label-check" onClick={darkMode}>
+              <i className="fa-solid fa-sun" ></i>
+              <i className="fa-solid fa-moon" ></i>
+              {/* <i className={dark ? "fa-solid fa-sun" : "fa-solid fa-moon"}></i> */}
             </label>
             {/* <button className="contact-button">
               <a href="#contact">Contacto</a>
@@ -432,12 +440,6 @@ function App() {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path
-                  d="M7.5 2C7.77614 2 8 2.22386 8 2.5L8 11.2929L11.1464 8.14645C11.3417 7.95118 11.6583 7.95118 11.8536 8.14645C12.0488 8.34171 12.0488 8.65829 11.8536 8.85355L7.85355 12.8536C7.75979 12.9473 7.63261 13 7.5 13C7.36739 13 7.24021 12.9473 7.14645 12.8536L3.14645 8.85355C2.95118 8.65829 2.95118 8.34171 3.14645 8.14645C3.34171 7.95118 3.65829 7.95118 3.85355 8.14645L7 11.2929L7 2.5C7 2.22386 7.22386 2 7.5 2Z"
-                  fill="currentColor"
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                ></path>
               </svg>
             </div>
           </div>
