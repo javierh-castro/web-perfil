adb7564ed455fe57a2c3277a8d5a08020eef91ea
javierh-castro <javi2cc1@gmail.com>
2024-05-14 18:25:34 -0300

    Integrando responcive

--- src/App.tsx
+++ src/App.tsx
@@ -321,9 +321,21 @@ function App() {
                   className="bi bi-linkedin"
                 ></a>
               </p>
-              <a className="cv" download="CV" href="./CV javier.pdf">
+              <div>
+              <div className="group relative">
+  <button>
+  <svg stroke-linejoin="round" stroke-linecap="round" stroke-width="2" stroke="currentColor" fill="none" viewBox="0 0 24 24" className="w-8 hover:scale-125 duration-200 hover:stroke-blue-500"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+  </button>
+  <span className="absolute -top-14 left-[50%] -translate-x-[50%] 
+  z-20 origin-left scale-0 px-3 rounded-lg border 
+  border-gray-300 bg-white py-2 text-sm font-bold
+  shadow-md transition-all duration-300 ease-in-out 
+  group-hover:scale-100">GitHub<span>
+</span></span></div>
+              </div>
+              {/* <a className="cv" download="CV" href="./CV javier.pdf">
                 CV
-              </a>
+              </a> */}
             </div>
           </div>
           {/* <NavComponent/> */}
@@ -339,8 +351,9 @@ function App() {
               <div className="description">
                 <h2>Recipify</h2>
                 <p>
-                  En este proyecto echo en equipo basado en next.js, es una web donde te registras y buscas recetas de cocina también podes publicar tus
-                  recetas en la web
+                  En este proyecto echo en equipo basado en next.js, es una web
+                  donde te registras y buscas recetas de cocina también podes
+                  publicar tus recetas en la web
                 </p>
               </div>
               <picture className="picture">
